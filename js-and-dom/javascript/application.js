699aa90004842a95816cbe39c4fd8baf8a07c6ba
Carlos Baez Fumero <carlos.baez.fumero@gmail.com>
2024-02-09 10:07:10 +0100

    dom and eventlisteners

---
+++ js-and-dom/javascript/application.js
@@ -0,0 +1,60 @@
+// console.log("Hello from JavaScript!");
+
+// // const beatles = ["paul", "john", "ringo", "george"];
+
+// // beatles.forEach(beatle => {
+// //   console.log(beatle)
+// // });
+
+// const abba = ['Bjorn', 'Benny', 'Anni-frid', 'Agnetha'];
+// let liContent = ''
+
+// abba.forEach(member => {
+//   // console.log(member)
+//   liContent += `<li>${member}</li>`
+//   // console.log(liContent)
+// });
+
+// const ulContent = `<ul>${liContent}</ul>`
+
+// document.body.insertAdjacentHTML("beforeend", ulContent)
+
+// const allMembers = document.querySelectorAll('li')
+// console.log(allMembers)
+
+// allMembers.forEach((member) => {
+//   member.innerText += '🎵'
+// })
+
+// const ul = document.querySelector('ul')
+// ul.style.listStyleType = "upper-roman";
+
+// const instrument = document.querySelector('li').dataset.instrument;
+// // console.log(instrument)
+
+// const members = document.querySelectorAll('li')
+// // console.log(members)
+
+// members.forEach(member => {
+//   member.innerText += `: ${member.dataset.instrument}`
+// });
+
+// const romain = document.getElementById("romain");
+// romain.addEventListener("click", (event) => {
+//   // console.log(event);
+//   console.log(event.currentTarget);
+//   event.currentTarget.classList.toggle("img-circle");
+// });
+
+// const button = document.querySelector('.btn-light')
+// console.log(button)
+
+// button.addEventListener("click", (event) => {
+//   // console.log(event)
+//   const checkboxes = document.querySelectorAll('.form-check-input')
+//   // console.log(checkboxes)
+//   checkboxes.forEach((checkbox) => {
+//     console.dir(checkbox.checked)
+//     checkbox.checked = !checkbox.checked;
+//   })
+// });
